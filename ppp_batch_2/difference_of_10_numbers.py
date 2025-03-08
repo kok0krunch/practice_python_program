e5c8561d27020e9503e51dc5f35f25596e5c14a2
Amalia Kadoi <amaliakadoi@gmail.com>
2025-03-08 12:51:58 +0800

    Moved from another repository
    Renamed the folder and files to improve organization and clarity

---
+++ ppp_batch_2/difference_of_10_numbers.py
@@ -0,0 +1,11 @@
+# Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
+
+first_num = int(input("Enter number 1: "))
+
+minus_allnum = first_num
+
+for i in range(2,11):
+    num = int(input(f"Enter number {i}: "))
+    minus_allnum -= num
+
+print(f"The result of the first number minus all numbers is: {minus_allnum}.")
---
+++ ppp_batch_2/not_equal.py
@@ -0,0 +1,10 @@
+# Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+if num1 != num2:
+    print("Both number are Not Equal.")
+else:
+    print("Both numbers are Equal.")
+    
