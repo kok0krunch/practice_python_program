b2bb2960c9680ef58fad6aeed945807d002d6719
kok0krunch <amaliakadoi@gmail.com>
2025-03-11 00:00:26 +0800

    Print the sum of two numbers

---
+++ batch_1/prog03_sum_of_two_numbers.py
@@ -0,0 +1,8 @@
+# Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+sum = num1 + num2
+
+print(f"The sum of {num1} and {num2} is {sum}.")
