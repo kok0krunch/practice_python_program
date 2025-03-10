115d1d78a5c13fee6a425116f4e0b4cdbf067384
kok0krunch <amaliakadoi@gmail.com>
2025-03-11 00:05:53 +0800

    Print the quotient of two numbers with decimal point

---
+++ batch_1/prog05_quotient_with_decimal_point.py
@@ -0,0 +1,8 @@
+# Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+result = num1 / num2
+
+print(f"The quotient of {num1} and {num2} is {result}.")
