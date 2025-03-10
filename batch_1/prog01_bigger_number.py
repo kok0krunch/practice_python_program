49154a031d89f7f8b25e2247db7f5425d4e6d717
kok0krunch <amaliakadoi@gmail.com>
2025-03-10 23:40:43 +0800

    Printing the bigger number

---
+++ batch_1/prog01_bigger_number.py
@@ -0,0 +1,8 @@
+# Create a program that ask user to input 2 numbers. Print the bigger number.
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second numer: "))
+
+bigger_num = max(num1, num2)
+
+print(f"The bigger number is: {bigger_num}.")
