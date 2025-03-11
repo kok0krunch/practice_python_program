eb22f5a50636e18d64a3f0fab402d2828f446ea3
kok0krunch <amaliakadoi@gmail.com>
2025-03-11 22:41:26 +0800

    Print the remainder of the first number divided by the second

---
+++ batch_2/prog05_remainder_of_two_numbers_when_divided.py
@@ -0,0 +1,8 @@
+# Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+result = num1 % num2
+
+print(f"The remainder is {result}.")
