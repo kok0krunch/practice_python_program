c4cb7618b775425659f7dfc6058115a3342afa40
kok0krunch <amaliakadoi@gmail.com>
2025-03-11 22:45:51 +0800

    Print the count of even numbers in 10 numbers

---
+++ batch_2/prog07_even_numbers_in_10_numbers.py
@@ -0,0 +1,11 @@
+# Create a program that ask user to input 10 numbers. Print how many are even numbers.
+
+even_count = 0
+
+for i in range(1,11):
+    num = int(input(f"Enter number {i}: "))
+   
+    if num % 2 == 0:
+        even_count += 1
+       
+print(f"The number of even numbers is {even_count}.")
