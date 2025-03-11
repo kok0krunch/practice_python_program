6dd799ecbbeb3e8af40064aac5c346d52a4b6ae2
kok0krunch <amaliakadoi@gmail.com>
2025-03-11 22:35:55 +0800

    Print the difference of two numbers

---
+++ batch_2/prog03_difference_of_two_numbers.py
@@ -0,0 +1,8 @@
+# Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+difference = num1 - num2
+
+print(f"The difference of {num1} and {num2} is {difference}.")
