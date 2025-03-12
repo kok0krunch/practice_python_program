16a5c4737fefe2ed7123968e18bd7425d051839c
kok0krunch <amaliakadoi@gmail.com>
2025-03-12 21:29:43 +0800

    Checks if numbers are duplicate or unique

--- batch_4/prog01_duplicate_numbers.py
+++ batch_4/prog01_duplicate_numbers.py
@@ -5,3 +5,11 @@ numbers = []
 for i in range(1, 11):
     num = int(input(f"Enter number {i}: "))
     numbers.append(num)
+    
+duplicates = set()
+seen = set()
+for num in numbers:
+    if num in seen:
+        duplicates.add(num)
+    else:
+        seen.add(num)
