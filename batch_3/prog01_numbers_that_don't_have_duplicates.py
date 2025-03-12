ab3b6d2e603a05f116f9b10db70570b07d55f7e2
kok0krunch <amaliakadoi@gmail.com>
2025-03-12 19:09:23 +0800

    Display numbers that don't have duplicates

--- batch_3/prog01_numbers_that_don't_have_duplicates.py
+++ batch_3/prog01_numbers_that_don't_have_duplicates.py
@@ -5,5 +5,10 @@ numbers = []
 for i in range(1, 11):
     num = int(input(f"Enter number {i}: "))
     numbers.append(num)
-
+    
 unique_numbers = [num for num in numbers if numbers.count(num) == 1]
+
+if unique_numbers:
+    print ("Numbers that don't have duplicates:", unique_numbers)
+else:
+    print ("All numbers are duplicates.")
