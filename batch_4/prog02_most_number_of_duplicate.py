004a18d2c2bd4ff74e42729938c08fe3ad1f5014
kok0krunch <amaliakadoi@gmail.com>
2025-03-12 21:45:59 +0800

    Identifying the number with most duplicates

--- batch_4/prog02_most_number_of_duplicate.py
+++ batch_4/prog02_most_number_of_duplicate.py
@@ -10,3 +10,10 @@ while True:
         numbers.append(num)
     except ValueError:
         break
+    
+if numbers:
+    count = Counter(numbers)
+    most_number = count.most_common(1)[0]
+    print(f"The number with the most duplicate is {most_number[0]}, entered {most_number[1]} times.")
+else:
+    print("All number are unique")
