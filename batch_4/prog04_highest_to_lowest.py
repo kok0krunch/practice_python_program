e7b76600e4f44e8195c80a0b9e87c3f3b2a30e33
kok0krunch <amaliakadoi@gmail.com>
2025-03-12 21:57:02 +0800

    Ask users to input a number until invalid

---
+++ batch_4/prog04_highest_to_lowest.py
@@ -0,0 +1,10 @@
+# Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from highest to lowest. Clue: sort() function
+
+numbers = []
+
+while True:
+    try:
+        num = int(input("Enter a number: "))
+        numbers.append(num)
+    except ValueError:
+        break
